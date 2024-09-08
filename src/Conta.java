d65df732fba4746204fd6432e84fa2443759c877
Emanuel Marinho <emanuel_sales12@hotmail.com>
2024-09-08 12:45:18 -0300

    Exercicio02

---
+++ src/Conta.java
@@ -0,0 +1,32 @@
+public class Conta {
+    public String cpf;
+    public int banco;
+    public double saldo;
+    public boolean correntistaEspecial;
+
+    public double bonificarCorrentista() {
+        if(correntistaEspecial) {
+            return this.saldo + (this.saldo * 0.10);
+        } else {
+            return this.saldo + (this.saldo * 0.05);
+        }
+    }
+
+    public void sacar(double valor) {
+        if (valor > this.saldo) {
+            System.out.println("Saldo insuficiente");
+        } else {
+            this.saldo -= valor;
+            System.out.println("Saque feito com sucesso!");
+        }
+    }
+
+    public void depositar(double valor) {
+        this.saldo += valor;
+        System.out.println("Depósito feito com sucesso!");
+    }
+
+    public void mostrarAtributos() {
+    System.out.println("Cpf: " + this.cpf + "\nBanco: " + this.banco + "\nSaldo: "  + this.saldo);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Conta conta1 = new Conta();
+        conta1.cpf = "123";
+        conta1.banco = 1;
+        conta1.saldo = 1000.0;
+        conta1.sacar(2000.0);
+        conta1.depositar(1000.0);
+        conta1.mostrarAtributos();
+        System.out.println(conta1.bonificarCorrentista());
+        System.out.println("\n\n\n");
+        Conta conta2 = new Conta();
+        conta2.cpf = "234";
+        conta2.banco = 2;
+        conta2.saldo = 1000.0;
+        conta2.correntistaEspecial = true;
+
+        System.out.println(conta2.bonificarCorrentista());
+    }
+}
